7ec6f74a4305719d404bacb52ecc67dd1aea830f
kirillzyusko <zyusko.kirik@gmail.com>
2021-03-01 21:02:52 +0300

    [RNBSE-005] - metro bundler configuration

--- metro.config.js
+++ metro.config.js
@@ -4,30 +4,56 @@
  *
  * @format
  */
-const path = require('path');
 const blacklist = require('metro-config/src/defaults/blacklist');
 
-module.exports = {
+const resolve = require('path').resolve;
+const fs = require('fs');
+
+// Update the following line if the root folder of your app is somewhere else.
+const ROOT_FOLDER = resolve(__dirname, '.');
+
+const config = {
+  transformer: {
+    getTransformOptions: (_, {platform}) => {
+      let modulePaths = [];
+      const moduleMap = {};
+
+      if (platform === 'android') {
+        modulePaths = require('./packager/modules.android');
+      } else if (platform === 'ios') {
+        modulePaths = require('./packager/modules.ios');
+      } else if (platform === 'macos') {
+        modulePaths = require('./packager/modules.macos');
+      } else {
+        modulePaths = require('./packager/modules.windows');
+      }
+
+      modulePaths.forEach((path) => {
+        if (fs.existsSync(path)) {
+          moduleMap[resolve(path)] = true;
+        }
+      });
+
+      return {
+        preloadedModules: moduleMap,
+        transform: {inlineRequires: {blacklist: moduleMap}},
+      };
+    },
+  },
   resolver: {
     blacklistRE: blacklist([
       // This stops "react-native run-windows" from causing the metro server to crash if its already running
-      new RegExp(
-        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
-      ),
+      new RegExp(`${resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`),
       // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
       new RegExp(
-        `${path
-          .resolve(__dirname, 'msbuild.ProjectImports.zip')
-          .replace(/[/\\]/g, '/')}.*`,
+        `${resolve(__dirname, 'msbuild.ProjectImports.zip').replace(
+          /[/\\]/g,
+          '/',
+        )}.*`,
       ),
     ]),
   },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
+  projectRoot: ROOT_FOLDER,
 };
+
+module.exports = config;
